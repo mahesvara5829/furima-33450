8900181b04c00abb11b17b9c58ef08254d6bc996
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-05 22:34:34 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
   end
 
   describe "ユーザー新規登録" do
+    context "新規登録に失敗する" do
     it "nicknameが空だと登録できない" do
       @user.nickname = ""
       @user.valid?
@@ -59,34 +60,33 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    it "ユーザー名は名字と名前が両方ないと登録できない" do
+    it "ユーザー名は名字がないと登録できない" do
       @user.last_name=""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it "ユーザー名は名前がないと登録できない" do
       @user.first_name=""
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last name can't be blank", "First name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
 
-    it "ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力があれば登録できる" do
-      expect(@user).to be_valid
-    end
 
 
-    it "ユーザー本名のフリガナは、名字と名前両方ないと登録できない" do
+
+    it "ユーザー本名のフリガナは、名字がないと登録できない" do
       @user.last_name_kana=""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end
+
+    it "ユーザー本名のフリガナは、名前がないと登録できない" do
       @user.first_name_kana=""
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last name kana can't be blank", "First name kana can't be blank")
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end
-
-
-    it "ユーザー本名のフリガナは、全角（カタカナ）での入力出ないと登録できない" do
-      @user.last_name_kana="もり"
-      @user.first_name_kana="もり"
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is invalid")
-    end
-
 
     it "生年月日が必須であること" do
       @user.birthday=""
@@ -94,8 +94,34 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
 
+  end
+
+  context "新規登録に成功する" do
 
 
+    it "ユーザーの名字は、全角（漢字・ひらがな・カタカナ）での入力であれば登録できる" do
+      @user.last_name="森"
+      expect(@user).to be_valid
+    end
+
+    it "ユーザーの名前は、全角（漢字・ひらがな・カタカナ）での入力であれば登録できる" do
+      @user.first_name="森"
+      expect(@user).to be_valid
+    end
+    
+
+
+    it "ユーザーの名字のフリガナは、全角（カタカナ）での入力であれば登録できる" do
+      @user.last_name_kana="モリ"
+      expect(@user).to be_valid
+    end
+
+    it "ユーザーの名前のフリガナは、全角（カタカナ）での入力であれば登録できる" do
+      @user.first_name_kana="モリ"
+      expect(@user).to be_valid
+    end
+
+  end
 
 
   
