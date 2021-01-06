06e71f26a8f2fa6d4264062d61059812937c41a3
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-06 20:05:10 +0900

    出品機能バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :status
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :send_fee
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :send_area
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :send_day
+  belongs_to :category,:status,:send_fee,:send_area,:send_day
+
 
   has_one_attached :image
   belongs_to :user
 
-  validates :image,presence: true
-  validates :name,presence: true
-  validates :discription,presence: true
-  validates :category_id,presence: true, numericality: { other_than: 1 }
-  validates :status_id,presence: true, numericality: { other_than: 1 }
-  validates :send_fee_id,presence: true, numericality: { other_than: 1 }
-  validates :send_area_id,presence: true, numericality: { other_than: 1 }
-  validates :send_day_id,presence: true, numericality: { other_than: 1 }
-  validates :price,presence: true
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :discription
+    validates :category_id,:status_id,:send_fee_id,:send_area_id,:send_day_id, numericality: { other_than: 1 }
+    validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  end
+
+
 
 
 end
 
 
-#,in:300..9999999,format:{with:/\A[0-9]+\z/}
