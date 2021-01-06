fd47418c16894d6ff08c6deb9bee877672ae5983
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-06 11:41:26 +0900

    テストコード再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,28 @@ class User < ApplicationRecord
          has_many :items
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
          validates :birthday, presence: true
 
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
     validates :password, presence: true,
             format: { with: VALID_PASSWORD_REGEX}
+
+          VALID_LAST_NAME_REGEX =/\A[ぁ-んァ-ン一-龥]/
+            validates :last_name, presence: true,
+                    format: { with: VALID_LAST_NAME_REGEX}
+
+                    VALID_FIRST_NAME_REGEX =/\A[ぁ-んァ-ン一-龥]/
+                    validates :first_name, presence: true,
+                            format: { with: VALID_FIRST_NAME_REGEX}    
+
+                            VALID_LAST_NAME_KANA_REGEX =/\A[ぁ-んァ-ン一-龥]/
+                    validates :last_name_kana, presence: true,
+                            format: { with: VALID_LAST_NAME_KANA_REGEX}                  
+
+                            VALID_FIRST_NAME_KANA_REGEX =/\A[ぁ-んァ-ン一-龥]/
+                    validates :first_name_kana, presence: true,
+                            format: { with: VALID_FIRST_NAME_KANA_REGEX}                  
+
 
   
 end
