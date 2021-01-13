0bd8641f57ee49a6d2996a4f68d1a72e89dc38f1
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-13 17:46:48 +0900

    ユーザー認証、表示変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_address=OrderAddress.new
-    if current_user.id==@item.user_id
+    if current_user.id==@item.user_id || @item.order!=nil
       redirect_to root_path
     end
   end
